7c3a262f32136d7fcbdfbdfe1716ad5b0ab6c42a
karthiongitkanini <57354284+karthiongitkanini@users.noreply.github.com>
2020-01-04 11:16:27 +0530

    1st

---
+++ FlightBookingWepApiProject/ApplicationOAuthProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using FlightBookingWepApiProject.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FlightBookingWepApiProject
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDbContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = await manager.FindAsync(context.UserName, context.Password);
+            if (user != null)
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("Username", user.UserName));
+                identity.AddClaim(new Claim("Lastname", user.Lastname));
+                identity.AddClaim(new Claim("Firstname",user.Firstname));
+              //  identity.AddClaim(new Claim("Email", user.Email));
+                context.Validated(identity);
+            }
+            else
+                return;
+        }
+    }
+}
